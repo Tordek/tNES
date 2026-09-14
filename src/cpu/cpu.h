13abec6841b861cd909a54a919f236d659a0b959
Guillermo O. «Tordek» Freschi <kedrot@gmail.com>
2026-09-14 14:11:05 -0300

    More instructions

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -320,6 +320,8 @@ typedef struct ic_6502_registers
   uint8_t op;
   uint8_t zp;
   uint16_t address;
+  uint16_t address_unfixed;
+  bool page_jump;
 } ic_6502_registers;
 
 /**
@@ -341,6 +343,7 @@ enum ui_address
   UI_ADDR_ZP,
   UI_ADDR_ZP_PLUS_1,
   UI_ADDR_TEMP,
+  UI_ADDR_TEMP_UNFIX,
   UI_ADDR_SP,
   UI_ADDR_SP_INC,
 };
@@ -395,8 +398,8 @@ enum ui_alu_op
   UI_ALU_RRA,
   UI_ALU_INC,
   UI_ALU_DEC,
-  UI_ALU_TRY_ADDY,
   UI_ALU_ADDY,
+  UI_ALU_INC_ADDR,
   UI_ALU_SHIFT,
   UI_ALU_BRANCH_CHECK,
   UI_ALU_BRANCH_TRY,
